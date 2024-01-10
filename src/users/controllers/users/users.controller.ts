77fb6f6e6ff0bb51215f0654cea2443ea11f6c08
mustafa sahin <mustafa.sahin@kgteknoloji.com>
2024-01-10 14:34:31 +0300

    first commit

---
+++ src/users/controllers/users/users.controller.ts
@@ -0,0 +1,42 @@
+import { CreateUserDto } from '@app/users/dtos/CreateUser.dto';
+import { UpdateUserDto } from '@app/users/dtos/UpdateUser.dto';
+import { UsersService } from '@app/users/services/users/users.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  constructor(private userService: UsersService) {}
+
+  @Get()
+  async getUsers() {
+    const users = await this.userService.getUsers();
+    return users;
+  }
+
+  @Post()
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Put(':id')
+  async updateUserById(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    await this.userService.updateUser(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  async deleteUserById(@Param('id', ParseIntPipe) id: number) {
+    await this.userService.deleteUser(id);    
+  }
+}
---
+++ src/users/services/users/users.service.ts
@@ -0,0 +1,32 @@
+import { User } from '@app/typeorm/entities/User';
+import { CreateUserParams, UpdateUserParams } from '@app/utils/types';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  getUsers() {
+    return this.userRepository.find();
+  }
+
+  createUser(userDetails: CreateUserParams) {
+    const newUser = this.userRepository.create({
+      ...userDetails,
+      createdAt: new Date(),
+    });
+    this.userRepository.save(newUser);
+  }
+
+  updateUser(id: number, userDetails: UpdateUserParams) {
+    return this.userRepository.update({ id }, { ...userDetails });
+  }
+
+  deleteUser(id: number) {
+    return this.userRepository.delete({ id });
+  }
+}
